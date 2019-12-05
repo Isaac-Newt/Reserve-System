ee705e5ff7b57fc280e769060ef06006dbca81c7
Isaac-Newt <isaac.newt.list@icloud.com>
2019-12-05 10:52:04 -0600

    finish Key, get data members and constructor for CirculatingItem

--- src/ReserveSystem/src/reserve_system/CirculatingItem.java
+++ src/ReserveSystem/src/reserve_system/CirculatingItem.java
@@ -2,4 +2,22 @@ package reserve_system;
 
 public abstract class CirculatingItem {
 	
+	// Abstract class does not define data members, only declares them
+	private int Barcode;
+	private int loanDuration;
+	private String Location;
+	private CheckoutRecord Record;
+	
+	public CirculatingItem(int bar, int duration, String loc, CheckoutRecord rec) {
+		// Default constructor, call from subclasses with super(parameters)
+		this.Barcode = bar;
+		this.loanDuration = duration;
+		this.Location = loc;
+		this.Record = rec;
+	}
+	
+	public void setRecord(Patron patron) {
+		// Use Record data member's setters to modify dueDate and patronID
+	}
+	
 }
--- src/ReserveSystem/src/reserve_system/Key.java
+++ src/ReserveSystem/src/reserve_system/Key.java
@@ -2,4 +2,12 @@ package reserve_system;
 
 public class Key extends CirculatingItem {
 
+	private int roomNumber;
+	
+	public Key(int bar, CheckoutRecord rec, int room) {
+	    // rooms are checked out for 2 hours, and are located in the key drawer
+	    super(bar, 2, "Key Drawer", rec);
+	    this.roomNumber = room;
+	}
+	
 }
