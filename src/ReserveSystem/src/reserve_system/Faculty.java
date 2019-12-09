05f599322ee752f37ac1ee1825ff3e4b7876177f
Isaac-Newt <isaac.newt.list@icloud.com>
2019-12-08 18:52:48 -0600

    remove subclasses of patron

--- src/ReserveSystem/src/reserve_system/Faculty.java
+++
@@ -1,9 +0,0 @@
-package reserve_system;
-
-public class Faculty extends Patron {
-	
-	public Faculty(int i, String ln, String fn, String e, int p) {
-		super(i, ln, fn, e, p);
-	}
-	
-}
--- src/ReserveSystem/src/reserve_system/Student.java
+++
@@ -1,8 +0,0 @@
-package reserve_system;
-
-public class Student extends Patron {
-	public Student(int i, String ln, String fn, String e, int p){
-		super(i, ln, fn, e, p);
-	}
-
-}
