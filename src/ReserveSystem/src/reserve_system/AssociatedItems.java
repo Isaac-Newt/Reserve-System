21793fbac5edc5c71207480664c4c79f4c7739b8
erdete01 <44326790+erdete01@users.noreply.github.com>
2019-12-10 02:46:07 -0600

    Add files via upload

--- src/ReserveSystem/src/reserve_system/AssociatedItems.java
+++ src/ReserveSystem/src/reserve_system/AssociatedItems.java
@@ -1,40 +1,71 @@
 package reserve_system;
 
-//import javax.swing.*;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-class AssociatedItems {
-    Accessory[] items;
+import javax.swing.*;
 
-    public void buildList(Accessory[] accessory, CheckoutRecord checkout) {
-        this.items = accessory;
-        //variable to hold list of strings
-        String str = "";
-        //loop to iterate through all the accessory items
+class AssociatedItems extends JFrame implements ActionListener {
+    
+	
+	Accessory[] items;
+    // Adding items to an empty string
+    public void buildList(Accessory[] accessory, CheckoutRecord checkout) 
+    {
+    	this.items = accessory;
+    	String str = "";
+    	//loop to iterate through all the accessory items
         for (int i = 0; i < items.length; i++) {
             //creating list of strings by concatenating
             if (items[i] != null) {
-                str += items[i].name + "\n";
+                str += items[i].getName() + "\n";
             }
         }
-        removeItemsAtCheckout(displayDialog(str)); 
     }
-
-//    public String displayDialog(String str) {  // MY GUI
-//    }
-
-    // This function It will remove the item when ever your enter a 
-    // list item in the message box
     
-    public void removeItemsAtCheckout(String name) {
-        boolean check = false;
-        for (int i = 0; i < items.length; i++) {
-            if (items[i].name.equalsIgnoreCase(name)) {
-                items[i] = null;
-                check = true;
-            }
-        }
-        if (check == true) {
-            buildList(items, this.checkout);
-        }
+    // Removing Items
+    public void removeItemsAtCheckout(String name) {}
+    
+    // GUI Part
+    JTextField tf; JFrame frame; JButton b; JLabel l;
+    AssociatedItems()
+    {
+        // Setting the button 
+        JButton b = new JButton("Check Out");
+        b.setBounds(130, 100, 100, 40);
+        b.addActionListener(new ActionListener() {
+													@Override
+													public void actionPerformed(ActionEvent e) {
+														// Clear the List and Accept 
+													}
+        										  });
+        // Setting the JTextField
+        tf = new JTextField();
+        tf.setBounds(80, 50, 100, 40);
+        
+        // Setting the JLabels
+        l = new JLabel();
+        l.setBounds(50, 150, 100, 40);
+        
+        add(b); add(tf);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        frame.setFocusable(true);
+    	
     }
+    
+    public static void main(String[] args) 
+    {
+    	new AssociatedItems();
+    }
+    
+	@Override // Inherit ActionListener class
+	public void actionPerformed(ActionEvent e) {
+		String text = tf.getText();
+		//pass
+	}
+  
+
 }
